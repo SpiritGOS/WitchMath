282313fa66eb071418123eb9e100033939611f2f
DanteMoren <73705466+DanteMoren@users.noreply.github.com>
2021-10-04 02:24:38 +0300

    вращение находит правильные собственные значения и какую-то муть с векторами

--- Rotations/Rotations.py
+++ Rotations/Rotations.py
@@ -1,13 +1,10 @@
 from math import atan, cos, sin
 
 def rotation_method(matrix, eps):
-    eps = str(eps).count('0')
-    print(eps)
     rotated = [[1 if row == col else 0 for row in range(len(matrix))] for col in range(len(matrix))]
-    return rotations(matrix, rotated, 2)
+    return rotations(matrix, rotated, eps)
 
 def rotations(matrix, rotated_matrix, eps):
-    print(f'EPS IS {eps}')
     def printMatrix(matrix):
         for a in range(len(matrix)):
             for b in range(len(matrix[a])):
@@ -15,7 +12,8 @@ def rotations(matrix, rotated_matrix, eps):
             print('\n', end='')
         print('\n', end='')
     
-    def transpon(matrix, n):
+    def transpon(matrix):
+        n = len(matrix)
         transpon_matrix = []
         for i in range(n):
             transpon_matrix.append([0]*n)
@@ -26,7 +24,7 @@ def rotations(matrix, rotated_matrix, eps):
     
     def multiply_matrix(A, B):
         length = len(A) 
-        result_matrix = [[0 for i in range(length)] for i in range(length)]
+        result_matrix = [[0.0 for i in range(length)] for i in range(length)]
         for i in range(length):
             for j in range(length):
                 for k in range(length):
@@ -34,19 +32,16 @@ def rotations(matrix, rotated_matrix, eps):
         return result_matrix
     
     def final():
-        size = len(matrix)
+        size = len(rotated_matrix)
         lambdas = []
         for i in range(size):
             for j in range(size):
                 if i == j:
                     lambdas.append(matrix[i][j])
-        print('FINALLY')
-        print(*rotated_matrix, sep='\n')
-        print()
-        print(*matrix, sep='\n')
-        return (lambdas, matrix)
+        V = rotated_matrix
+        return (lambdas, V)
 
-    if eps == 0:
+    if eps >= 1:
        return final()
 
     print('Исходная матрица:')
@@ -56,24 +51,27 @@ def rotations(matrix, rotated_matrix, eps):
 
     
     for i in range(size):
-        for j in range(size):
-            if abs(matrix[i][j] > max_):
+        for j in range(i,size):
+            if i == j:
+                pass
+            elif abs(matrix[i][j] >= max_):
                 max_ = matrix[i][j]
                 k = i  # maxi по методе
                 m = j  # maxj по методе
+
     try:
         phi = 0.5 * atan((2 * matrix[k][m]) /
-                            (matrix[k][m] - matrix[k][m]))
+                            (matrix[k][k] - matrix[m][m]))
     except ZeroDivisionError:
         if matrix[k][m] >= 0:
-            phi = 0.78539816339
-        else:
             phi = -0.78539816339
+        else:
+            phi = 0.78539816339
 
     h_kk = cos(phi)
     h_mk = sin(phi)
     h_km = -sin(phi)
-    h_mm = -cos(phi)
+    h_mm = cos(phi)
 
     E = [[1 if i == j else 0 for i in range(size)] for j in range(size)]
         
@@ -83,12 +81,9 @@ def rotations(matrix, rotated_matrix, eps):
     H[m][k] = h_mk  
     H[k][m] = h_km
     H[m][m] = h_mm
-    printMatrix(H)
-    print()
-    Ht = transpon(H, size)
-    printMatrix(Ht)
+    Ht = transpon(H)
     matrix_1 = multiply_matrix(Ht, multiply_matrix(matrix, H))
-    rotations(matrix_1, multiply_matrix(rotated_matrix, H), eps - 1)
+    return rotations(matrix_1, multiply_matrix(rotated_matrix, H), eps*10)
 
 
 if __name__ == '__main__':
@@ -97,9 +92,10 @@ if __name__ == '__main__':
     with open('Matrix.txt', 'r') as f:
         for line in f:
             matrix.append([float(i) for i in line.split()])
+    # A = np.matrix(matrix)
+    # print(type(A))
+    # print(A)
     rotated = [[1 if row == col else 0 for row in range(len(matrix))] for col in range(len(matrix))]
-    print(rotation_method(matrix, eps))
-    # [
-    #     print(f'x[{i+1}] = {x}')
-    #     for i, x in enumerate(rotations(matrix, eps))
-    # ]
+    lambdas, V = rotation_method(matrix, eps)
+    [print(f'lambda[{i + 1}] = {l}') for i, l in enumerate(lambdas)]
+    [print(f'x[{i + 1}] = {l}') for i, l in enumerate(V)]
