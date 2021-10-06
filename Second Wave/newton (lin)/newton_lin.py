7fd4e66d2ba36db7576fc7fbf1bffe4aa610b13f
Marsel <marsel290801@gmail.com>
2021-10-06 20:53:31 +0300

    d

---
+++ Second Wave/newton (lin)/newton_lin.py
@@ -0,0 +1,18 @@
+import sympy
+import numpy
+
+
+def Newton_Lin(equations):
+    vars = sympy.symbols('x y')
+    f = sympy.sympify(equations)
+    Jacob = sympy.zeros(len(f), len(vars))
+    for i, fi in enumerate(f):
+        for j, s in enumerate(vars):
+            Jacob[i, j] = sympy.diff(fi, s)
+
+    print(Jacob.tolist(), end='\n')
+
+
+if __name__ == "__main__":
+    equations = ['2*x - cos(y)', '2*y - exp(x) - x']
+    Newton_Lin(equations)
