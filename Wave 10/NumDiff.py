f4c5a9ae4cbf2ecd47d2dd819af12ba4b89ef713
Marsel <marsel290801@gmail.com>
2021-11-21 23:04:11 +0300

    Started developing NumDiff

---
+++ Wave 10/NumDiff.py
@@ -0,0 +1,10 @@
+import sympy as sp
+import numpy as np
+import matplotlib.pyplot as plt
+import sys
+import os
+sys.path.insert(0, "Wave 1\\Gauss")
+from Gauss import Gauss
+
+if __name__ == "__main__":
+    pass
